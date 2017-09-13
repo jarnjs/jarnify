8900b815156896e0fd3e79bfb18f162d8353bb32
Marcus Tallberg <marcus.tallberg@richen.fi>
2017-09-13 20:11:54 +0300

    updated language version to ES2015

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// Count all of the links from the Node.js build page 
-var jsdom = require("node-jsdom");
+const jsdom = require("node-jsdom")
  
 jsdom.env(
-  "http://www.richen.fi",
-  function (errors, window) {
-    console.log(window);
-  }
-);
+    "http://www.richen.fi", 
+    (errors, window) => console.log(window)
+)
