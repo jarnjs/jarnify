aa6786c1adde9e4bf74f8b347e1083d1e328f92d
Marcus Tallberg <marcus.tallberg@richen.fi>
2017-09-14 22:20:05 +0300

    added some scraping

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const jsdom = require("node-jsdom")
- 
-jsdom.env(
-    "http://www.richen.fi", 
-    (errors, window) => console.log(window)
-)
+const jsdom = require('node-jsdom')
+const urlsToScrape = ['http://www.richen.fi', 'http://www.hs.fi', 'https://flunssatutka.fi/']
+const domElementToCheck = 'div'
+
+
+function checkUrl(url, domElement) {
+
+    jsdom.env({
+        url: url,
+        scripts: ['https://code.jquery.com/jquery-2.1.1.js'],
+        loaded(errors, window) {
+            const elementCount = window.$(domElement).length
+            const pageTitle = window.$('title').text()
+            const domLength = window.$('*').html().length
+            console.log(`${pageTitle} has ${elementCount} ${domElement}-elements and a DOM-length of ${domLength} chars`)
+            }
+    })
+}
+
+for(let i=0;i<urlsToScrape.length;i++) {
+    checkUrl(urlsToScrape[i], 'div')
+}
