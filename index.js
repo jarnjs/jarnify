aeda3f79339e00f5bf79fb034a9b92d2fec8e848
Janne Huotari <janne@richen.fi>
2018-05-04 09:41:01 +0300

    search all img tags and replace image source with test image

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const jsdom = require('node-jsdom')
-//const urlsToScrape = ['http://www.richen.fi', 'http://www.hs.fi', 'https://flunssatutka.fi/']
-const urlsToScrape = ['http://www.richen.fi']
+const urlsToScrape = ['http://www.richen.fi', 'http://www.hs.fi', 'https://flunssatutka.fi/']
 const domElementToCheck = 'div'
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const imgUrl = 'https://media.riemurasia.net/albumit/mmedia/ym/y3v/jbsz/58767/1320622347.gif'
 
 app.get('/html',function(req,res) {
-    checkUrl(urlsToScrape[0],'div', function(html){
+    checkUrl(urlsToScrape[1],'img', function(html){
         res.send(html)
     })
 })
@@ -16,17 +16,18 @@ app.listen(port, function(){
     console.log(`server start on port ${port}`)
 })
 
-
 function checkUrl(url, domElement, callback) {
     jsdom.env({
         url: url,
         scripts: ['https://code.jquery.com/jquery-2.1.1.js'],
         loaded(errors, window) {
-            const elementCount = window.$(domElement).length
-            const pageTitle = window.$('title').text()
-            const domLength = window.$('*').html().length
-            const pageHtml = window.$('*').html()
-            callback(pageHtml)
+            const $ = window.$;
+            const element = $(domElement)
+            const pageHtml = $('*')
+            pageHtml.find('img').each(function(i,el) {
+                el.src = imgUrl
+            })
+            callback(pageHtml.html())
             //console.log(`${pageTitle} has ${elementCount} ${domElement}-elements and a DOM-length of ${domLength} chars`)
             }
     })
