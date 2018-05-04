f4bf87cf0721f023ebf8506d97eed2faa3961efd
Janne Huotari <janne@richen.fi>
2018-05-04 09:49:40 +0300

    tested adding bootstrap css from cdn to html

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const jsdom = require('node-jsdom')
-const urlsToScrape = ['http://www.richen.fi', 'http://www.hs.fi', 'https://flunssatutka.fi/']
+const urlsToScrape = ['http://www.iltalehti.fi', 'http://www.hs.fi', 'https://flunssatutka.fi/']
 const domElementToCheck = 'div'
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const imgUrl = 'https://media.riemurasia.net/albumit/mmedia/ym/y3v/jbsz/58767/1320622347.gif'
+const bsUrl = 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css'
 
 app.get('/html',function(req,res) {
-    checkUrl(urlsToScrape[1],'img', function(html){
+    checkUrl(urlsToScrape[0],'img', function(html){
         res.send(html)
     })
 })
@@ -24,6 +25,7 @@ function checkUrl(url, domElement, callback) {
             const $ = window.$;
             const element = $(domElement)
             const pageHtml = $('*')
+            pageHtml.find('head').append(`<link rel="stylesheet" href="${bsUrl}" type="text/css" />`)
             pageHtml.find('img').each(function(i,el) {
                 el.src = imgUrl
             })
